ecba918dc0150960feb2884d847f7ee337efef74
AymericSANNA <sanna.huet@gmail.com>
2023-11-14 15:30:35 +0100

    create app's first part architecture, add About, Contact, Gallery, Home and Sitemap pages + begining of global layout

---
+++ Front/src/app/Gallery/gallery_page.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const GalleryPage = () => {
+    return (
+        <div>
+            <h1>Welcome to my Next.js page with React!</h1>
+            <p>This is some example content.</p>
+        </div>
+    );
+};
+
+export default GalleryPage;
