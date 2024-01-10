5dda5eb5cbb3357a4cbff28aa50f87b579121690
AymericSANNA <sanna.huet@gmail.com>
2024-01-10 14:23:17 +0100

    discard footer, decorations and nav components + upgrade Next.js to v14

--- Front/src/app/page.js
+++ Front/src/app/page.js
@@ -1,11 +1,13 @@
 import Image from 'next/image'
 import styles from './page.module.css'
+import NavMobile from '../../components/Nav/NavMobile'
 
 export default function Home() {
   return (
     <main className={styles.main}> 
 
       <div className={styles.center}>
+
         <p> IllusionSeed </p>
       </div>
     </main>
