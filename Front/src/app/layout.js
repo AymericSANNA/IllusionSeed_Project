feecb13ab819a85df5e8658e8de5790dbd63ecfb
AymericSANNA <sanna.huet@gmail.com>
2024-01-27 18:47:34 +0100

    Create footer components and footer CSS files + add Link tag on footer nav + alt description to social media logo + code cleanse in Layout.js

--- Front/src/app/layout.js
+++ Front/src/app/layout.js
@@ -1,8 +1,6 @@
-import SocialButton from "../../components/Buttons/SocialButton";
+
 import Header from "../../components/Header/Header";
-import TikTok from "./Images/logo_tiktok.png";
-import Instagram from "./Images/logo_instagram.png";
-import Threads from "./Images/logo_threads.png";
+import Footer from "../../components/Footer/Footer";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
@@ -24,42 +22,9 @@ export default function RootLayout({ children }) {
 
         {children}
 
-        <footer className="footer-container">
-          <p className="footer-IllusionSeed-title">
-            <a href="/">HappySoul Illustrate</a>
-          </p>
-          <ul className="footer-social-button-list">
-            {SocialButton(
-              TikTok,
-              "https://www.tiktok.com/@illusionseed",
-              "Tiktok logo"
-            )}
-            {SocialButton(
-              Instagram,
-              "https://www.instagram.com/illusion__seed/?hl=fr"
-            )}
-            {SocialButton(Threads, "https://www.threads.net/@illusion__seed")}
-          </ul>
-          <nav className="footer-nav">
-            <ul className="footer-nav-sitemap">
-              <li>
-                <a href="/Home">Home</a>
-              </li>
-              <li>
-                <a href="/About">About</a>
-              </li>
-              <li>
-                <a href="/Gallery">Gallery</a>
-              </li>
-              <li>
-                <a href="/Contact">Contact</a>
-              </li>
-              <li>
-                <a href="/Sitemap">Sitemap</a>
-              </li>
-            </ul>
-          </nav>
-        </footer>
+        <Footer />
+
+        
       </body>
     </html>
   );
