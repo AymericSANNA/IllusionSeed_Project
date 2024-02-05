4c24d85a4d7e5442c7c52c24094c2192e20d5558
AymericSANNA <sanna.huet@gmail.com>
2024-02-05 17:03:44 +0100

    Working on Gallery page CSS: size of card, scroll animation, description position on image + blur background

--- Front/components/ImageCards/ImageCard.js
+++ Front/components/ImageCards/ImageCard.js
@@ -1,16 +1,20 @@
 import Image from 'next/image'
 import React from 'react'
+import './imageCard.css'
 
 function imageCard(name, altImage, title, description) {
     return (
-        <div>
+        <div className='card-container'>
             <Image 
+                    className='card-image'
                     src={name}
                     width={300} 
                     height={400} 
                     alt={altImage}/>
-            <h3>{title}</h3>
-            <p>{description}</p>
+            <div className='card-text-container'>
+                <h3 className='card-title'>{title}</h3>
+                <p className='card-description'>{description}</p>
+            </div>
         </div>
     )
 }
